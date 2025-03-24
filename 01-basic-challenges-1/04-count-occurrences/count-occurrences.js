88822671fc88f03379cc537bc689040ce7066a1e
Jake Samuels <samuels242@googlemail.com>
2025-03-24 21:12:13 +0000

    04-count-occurrences ✅

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,23 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let count = 0;
+
+  //   for (let i = 0; i < str.length; i++) {
+  //     if (str[i] === char) {
+  //       count++;
+  //     }
+  //   }
+
+  // ===== CASE INSENSITIVE =====
+  let lowerStr = str.toLowerCase();
+  let lowerChar = char.toLowerCase();
+
+  for (let i = 0; i < lowerStr.length; i++) {
+    if (lowerStr[i] === lowerChar) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = countOccurrences;
