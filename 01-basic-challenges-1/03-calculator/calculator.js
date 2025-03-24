7a3c9f65a29fbfb698248f3751f1b18ddfeba60e
Jake Samuels <samuels242@googlemail.com>
2025-03-24 21:05:34 +0000

    03-calculator ✅

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,37 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  let result;
+  // ==== IF STATEMENT =====
+  //   if (operator === "+") {
+  //     result = num1 + num2;
+  //   } else if (operator === "-") {
+  //     result = num1 - num2;
+  //   } else if (operator === "*") {
+  //     result = num1 * num2;
+  //   } else if (operator === "/") {
+  //     result = num1 / num2;
+  //   } else {
+  //     throw new Error('Invalid operator');
+  //   }
+
+  // ===== SWITCH STATEMENT =====
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    default:
+      throw new Error("Invalid operator");
+  }
+
+  return result;
+}
 
 module.exports = calculator;
