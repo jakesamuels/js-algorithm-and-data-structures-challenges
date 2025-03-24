0df2f782d2ccd04613d1faff200248d312f16164
Jake Samuels <samuels242@googlemail.com>
2025-03-24 21:38:59 +0000

    06-title-case ✅

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,22 @@
-function titleCase() {}
+function titleCase(str) {
+  // ===== FOR LOOP (Initial attempt) =====
+  const wordsArr = str.toLowerCase().split(" ");
+  const formattedWords = [];
+  for (let i = 0; i < wordsArr.length; i++) {
+    const formattedWord = wordsArr[i][0].toUpperCase() + wordsArr[i].slice(1);
+    formattedWords.push(formattedWord);
+  }
+  return formattedWords.join(" ");
+
+  // ===== FOR LOOP (More concise) =====
+  //   const wordsArr = str.toLowerCase().split(" ");
+  //   for (let i = 0; i < wordsArr.length; i++) {
+  //     wordsArr[i] = wordsArr[i][0].toUpperCase() + wordsArr[i].slice(1);
+  //   }
+  //   return wordsArr.join(" ");
+
+  // ===== REGEX =====
+  //   return str.replace(/\b\w/g, (match) => match.toUpperCase());
+}
 
 module.exports = titleCase;
