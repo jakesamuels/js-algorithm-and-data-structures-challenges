0be517c81e78980271c3203403ed77015f14892d
Jake Samuels <samuels242@googlemail.com>
2025-03-24 21:18:15 +0000

    05-find-max-number

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,17 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  // ===== USING METHODS (max) =====
+  //   return Math.max(...arr);
+
+  let maxNumber = arr[0];
+
+  // ===== USING FOR LOOP =====
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maxNumber) {
+      maxNumber = arr[i];
+    }
+  }
+
+  return maxNumber;
+}
 
 module.exports = findMaxNumber;
